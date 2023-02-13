2a1e615d7ef2983eaec69dbb65e5c635ecdee92f
tornikesamkharadze <tornikesamkharadzee@gmail.com>
2023-02-13 21:52:22 +0400

    change browser router to hashroter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route,HashRouter } from "react-router-dom";
+import {  Routes, Route,HashRouter } from "react-router-dom";
 import { Home, Personal, Experience, Education, Resume } from "./pages/index";
 
 function App() {
